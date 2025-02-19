8527319b6380745d9c575f1c4d82b906db66ef60
sla-ppy <sla-ppy-pm@protonmail.com>
2025-02-19 17:29:28 +0100

    change display, fix lower/uppercase, fix gamelogic

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,17 @@
-import java.util.Arrays;
-import java.util.Objects;
 import java.util.Random;
 import java.io.BufferedReader;
 import java.io.*;
 import java.util.ArrayList;
 
-public class main {
-    public static void fillRemainingGuesses (ArrayList<Character> remainingGuesses) {
-        char[] englishAlphabet = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-        for (char c : englishAlphabet) {
-            remainingGuesses.add(c);
-        }
-    }
-
+public class Main {
     public static void main(String[] args) {
         // rng 1 string and convert to char[]
-        String[] wordArray = {"stride", "retain", "coerce", "symbol", "person", "scream", "murder", "volume", "devote", "wander", "create", "flight", "rotate", "junior", "harbor"};
-        Random random = new Random();
-        int RNG = random.nextInt(1, wordArray.length);
-        char[] charArray = wordArray[RNG].toCharArray();
+        //String[] wordArray = {"stride", "retain", "coerce", "symbol", "person", "scream", "murder", "volume", "devote", "wander", "create", "flight", "rotate", "junior", "harbor"};
+        String[] wordArray = {"create"};
+        //Random random = new Random();
+        //int RNG = random.nextInt(1, wordArray.length);
+        //char[] charArray = wordArray[RNG].toCharArray();
+        char[] charArray = wordArray[0].toCharArray();
 
         // needed for taking input
         InputStreamReader InputObj = new InputStreamReader(System.in);
@@ -26,11 +19,9 @@ public class main {
 
         ArrayList<Character> incorrectGuesses = new ArrayList<Character>(); // we use arraylist instead of vectors
         ArrayList<Character> remainingGuesses = new ArrayList<Character>();
-        fillRemainingGuesses(remainingGuesses);
         char[] guessedChars = {'_', '_', '_', '_', '_', '_'};
 
         System.out.println("Hangman - Guess the 6 letter, lowercase only word!");
-        String clearScreen = "\r\n\r\n\r\n";
 
         boolean gameWon = false;
         while(!gameWon) {
@@ -42,7 +33,6 @@ public class main {
                 if (i == guessedChars.length -1) {
                     System.out.println("Game won!");
                     gameWon = true;
-                    break;
                 }
             }
             if (gameWon) {
@@ -51,7 +41,7 @@ public class main {
 
             // check if we have guesses remaining
             if (incorrectGuesses.size() == 10) {
-                System.out.println("You ran out of guesses. Game lost!");
+                System.out.println("You ran out of lives. Game lost!");
                 System.exit(0);
             }
 
@@ -60,6 +50,7 @@ public class main {
             char c = 'A';
             try {
                 c = (char) BufferObj.read();
+                c = Character.toLowerCase(c);
                 BufferObj.read(); // omit '\n' going after previous char
             } catch (IOException e) {        // expections in java must be handled
                 e.printStackTrace();
@@ -67,14 +58,19 @@ public class main {
 
             // check if guess was correct
             boolean guessedCorrectly = false;
-            for (int i = 0; i < charArray.length; i++) {
-                if (!guessedCorrectly) {
-                    if (c == charArray[i]) {
+            if (!guessedCorrectly) {
+                for (char value : charArray) {
+                    if (c == value) {
                         System.out.println("Your guess was correct!");
-                        guessedChars[i] = charArray[i];
                         guessedCorrectly = true;
-                        remainingGuesses.remove(Character.valueOf(c));
                         break;
+                    }
+                }
+            };
+            if (guessedCorrectly) {
+                for (int i = 0; i < charArray.length; i++) {
+                    if (c == charArray[i]) {
+                        guessedChars[i] = charArray[i];
                     }
                 }
             }
@@ -83,14 +79,15 @@ public class main {
             if (!guessedCorrectly) {
                 System.out.println("Your guess was incorrect!");
                 incorrectGuesses.add(c);
-                remainingGuesses.remove(Character.valueOf(c));
             }
 
             // game progress info
             System.out.println(guessedChars);
-            System.out.println("You can make " + (10 - incorrectGuesses.size()) + " more wrong guesses!");
-            System.out.println("Remaining guesses: " + remainingGuesses);
-            System.out.println(clearScreen);
+            System.out.println("Lives left: " + (10 - incorrectGuesses.size()));
+            if (!incorrectGuesses.isEmpty()) {
+                System.out.println(incorrectGuesses);
+            }
+            System.out.println("\r\n");
         }
     }
 }
