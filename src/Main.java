492d41c6a36a14f8665bd946c42d51dfb88afcc6
sla-ppy <sla-ppy-pm@protonmail.com>
2025-02-19 20:53:08 +0100

    refactor code, changed input to file reading

--- src/Main.java
+++ src/Main.java
@@ -1,44 +1,28 @@
-import java.util.Random;
-import java.io.BufferedReader;
 import java.io.*;
+import java.io.BufferedReader;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        // rng 1 string and convert to char[]
-        //String[] wordArray = {"stride", "retain", "coerce", "symbol", "person", "scream", "murder", "volume", "devote", "wander", "create", "flight", "rotate", "junior", "harbor"};
-        String[] wordArray = {"create"};
-        //Random random = new Random();
-        //int RNG = random.nextInt(1, wordArray.length);
-        //char[] charArray = wordArray[RNG].toCharArray();
-        char[] charArray = wordArray[0].toCharArray();
+        char[] chosenWord = Vocabulary.generate();
+        System.out.println(chosenWord);
+
+        // prepare guess structure equal in length to chosen word
+        StringBuilder string = new StringBuilder();
+        string.append("_".repeat(chosenWord.length));
+        char[] guessedChars = new char[chosenWord.length];
+        string.getChars(0, chosenWord.length, guessedChars, 0);
 
         // needed for taking input
         InputStreamReader InputObj = new InputStreamReader(System.in);
         BufferedReader BufferObj = new BufferedReader(InputObj);
 
-        ArrayList<Character> incorrectGuesses = new ArrayList<Character>(); // we use arraylist instead of vectors
-        ArrayList<Character> remainingGuesses = new ArrayList<Character>();
-        char[] guessedChars = {'_', '_', '_', '_', '_', '_'};
+        ArrayList<Character> incorrectGuesses = new ArrayList<>(); // we use arraylist instead of vectors
 
         System.out.println("Hangman - Guess the 6 letter, lowercase only word!");
 
         boolean gameWon = false;
         while(!gameWon) {
-            // check if we won already
-            for (int i = 0; i < guessedChars.length; i++) {
-                if (guessedChars[i] == '_') {
-                    break;
-                }
-                if (i == guessedChars.length -1) {
-                    System.out.println("Game won!");
-                    gameWon = true;
-                }
-            }
-            if (gameWon) {
-                break;
-            }
-
             // check if we have guesses remaining
             if (incorrectGuesses.size() == 10) {
                 System.out.println("You ran out of lives. Game lost!");
@@ -58,19 +42,18 @@ public class Main {
 
             // check if guess was correct
             boolean guessedCorrectly = false;
-            if (!guessedCorrectly) {
-                for (char value : charArray) {
-                    if (c == value) {
-                        System.out.println("Your guess was correct!");
-                        guessedCorrectly = true;
-                        break;
-                    }
+            for (char value : chosenWord) {
+                if (c == value) {
+                    System.out.println("Your guess was correct!");
+                    guessedCorrectly = true;
+                    break;
                 }
-            };
+            }
+            ;
             if (guessedCorrectly) {
-                for (int i = 0; i < charArray.length; i++) {
-                    if (c == charArray[i]) {
-                        guessedChars[i] = charArray[i];
+                for (int i = 0; i < chosenWord.length; i++) {
+                    if (c == chosenWord[i]) {
+                        guessedChars[i] = chosenWord[i];
                     }
                 }
             }
@@ -88,6 +71,18 @@ public class Main {
                 System.out.println(incorrectGuesses);
             }
             System.out.println("\r\n");
+
+            // check if we won already
+            for (int i = 0; i < guessedChars.length; i++) {
+                if (guessedChars[i] == '_') {
+                    break;
+                }
+                if (i == guessedChars.length -1) {
+                    System.out.println("Game won!");
+                    gameWon = true;
+                    break;
+                }
+            }
         }
     }
 }
---
+++ src/Vocabulary.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Vocabulary {
+    // the words contained in vocabulary.txt are 5-14 letter words, 10 of each length
+    public static char[] generate() {
+        // get strings from input file into ArrayList<String>
+        ArrayList<String> wordsData = new ArrayList<String>();
+        try (BufferedReader br = new BufferedReader(new FileReader("resources/vocabulary.txt"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                wordsData.add(line);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        // convert from ArrayList<String> to String[]
+        String[] words = wordsData.toArray(new String[0]);
+
+        // choose and return random word
+        Random random = new Random();
+        int RNG = random.nextInt(1, words.length);
+        return words[RNG].toCharArray();
+    }
+}
